e1a326ef40369499a251c9c2b919ad89862643b7
dieggsy <diegoamundo@protonmail.com>
2017-12-02 20:47:28 -0500

    Change requirements and bump version.

--- eterm-256color.el
+++ eterm-256color.el
@@ -5,9 +5,9 @@
 ;; URL: http://github.com/dieggsy/eterm-256color
 ;; Git-Repository: git://github.com/dieggsy/eterm-256color
 ;; Created: 2017-11-01
-;; Version: 0.3.7
+;; Version: 0.3.8
 ;; Keywords: faces
-;; Package-Requires: ((emacs "25") (xterm-color "1.6"))
+;; Package-Requires: ((emacs "24.4") (xterm-color "1.6"))
 
 ;; This file is not part of GNU Emacs.
 
